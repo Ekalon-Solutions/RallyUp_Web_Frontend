46ee31fef7e84132dc59cfacf344fb5a92e1c4b5
Jay Jain <jay4codes@gmail.com>
2025-12-09 20:58:06 +0530

    fix

--- components/modals/create-merchandise-modal.tsx
+++ components/modals/create-merchandise-modal.tsx
@@ -352,7 +352,6 @@ export function CreateMerchandiseModal({
                 <div className="space-y-2">
                   <Label htmlFor="price">Price *</Label>
                   <div className="relative">
-                    <DollarSign className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
                     <Input
                       id="price"
                       type="number"
