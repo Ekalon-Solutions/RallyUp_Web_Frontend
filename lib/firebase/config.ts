c9b1ac7e17e2cf6d6b9a56cf5a228cb90975c048
Jay Jain <jay4codes@gmail.com>
2026-01-20 19:44:50 +0530

    fix: firebase config keys moved to .env

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -8,14 +8,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQEG8YSOrsVuxUBSfIGHfEHoP4-jJI3TE",
-  authDomain: "rallyup-28019.firebaseapp.com",
-  projectId: "rallyup-28019",
-  storageBucket: "rallyup-28019.firebasestorage.app",
-  messagingSenderId: "575936971534",
-  appId: "1:575936971534:web:2f2c987d135929aa6a0d5f",
-  measurementId: "G-SRLNL9FQ0G"
-};
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
